9817ec2cbfc0ce34597e9ed1bacb15e9bc76cec2
Hadrien G <knights_of_ni@gmx.com>
2020-06-15 09:57:39 +0200

    Build in both C++14 and C++17 mode

--- build.sh
+++ build.sh
@@ -18,28 +18,35 @@ IFS=$'\n\t'
 #
 docker build --pull --tag root-dev-base -f Dockerfile.base .
 
-# Run the actual ROOT build
+# Build ROOT in both C++14 and C++17 mode
 #
-# This step cannot be made part of the Dockerfile above because it requires a
-# bind mount of the ROOT development source tree, and the Docker Build
-# Reproducibilty Strike Force won't let us do such an unclean thing.
+# We need both because ROOT 7 development is currently capped to C++14 only
+# while hists-mt currently requires at least C++17 to build.
 #
-docker run -v ~/Bureau/Programmation/root:/mnt/root:ro                         \
-           --name root-dev-cont                                                \
-           root-dev-base                                                       \
-           bash /root/ROOT-build/root-build-worker.sh
+for ROOT_CXX_STD in 14 17; do
+    # Run the actual ROOT build
+    #
+    # This step cannot be made part of the Dockerfile above because it requires
+    # a bind mount of the ROOT development source tree, and the Docker Build
+    # Reproducibilty Strike Force won't let us do such an unclean thing.
+    #
+    docker run -v ~/Bureau/Programmation/root:/mnt/root:ro                     \
+               --name root-dev-cont                                            \
+               root-dev-base                                                   \
+               bash /root/ROOT-build/root-build-worker.sh $ROOT_CXX_STD
 
-# If the ROOT build succeeded, we can now commit the root-dev image from the
-# root-dev-cont container that we just made...
-docker commit --change "CMD bash" root-dev-cont root-dev
+    # If the ROOT build succeeded, we can now commit the root-dev image from the
+    # root-dev-cont container that we just made...
+    docker commit --change "CMD bash" root-dev-cont root-dev:cxx${ROOT_CXX_STD}
 
-# ...and then we don't need that container anymore and can drop it
-docker container rm root-dev-cont
+    # ...and then we don't need that container anymore and can drop it
+    docker container rm root-dev-cont
+done
 
 # Alright, we're done
-echo "*** ROOT development container was built successfully ***"
+echo "*** ROOT development containers were built successfully ***"
 
-# NOTE: The output "root-dev" image preserves the CMake build cache, so that
+# NOTE: The output "root-dev" images preserve the CMake build cache, so that
 #       you can quickly re-run the build by just firing up a container in
 #       root-dev with the same bind mount as above, go to /root/ROOT-build,
 #       and run "ninja" and whatever else you'd like in there.
--- root-build-worker.sh
+++ root-build-worker.sh
@@ -1,11 +1,16 @@
 ####################### CONTAINER-SIDE ROOT BUILD SCRIPT #######################
 # This script is meant to run inside of the container. If you are trying to    #
 # build the container, you're probably looking for the "build.sh" script.      #
+#                                                                              #
+# The script takes one argument, which is the ROOT build's C++ standard mode.  #
 ################################################################################
 
 # Force bash to catch more errors
 set -euo pipefail
 IFS=$'\n\t'
+
+# Decode CLI args
+ROOT_CXX_STD=$1
 
 # Go to the build directory
 cd /root/ROOT-build
@@ -18,14 +23,14 @@ cd /root/ROOT-build
 # FIXME: Disabling "dev" mode, which implies -Werror, until ROOT master is
 #        warnings-free on GCC 10.
 #
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_STANDARD=14  -Dasimage=OFF \
-      -Dclad=OFF -Ddavix=OFF -Ddev=OFF -Dfail-on-missing=ON -Dfftw3=OFF        \
-      -Dfitsio=OFF -Dgdml=OFF -Dgfal=OFF -Dhttp=OFF -Dmlp=OFF -Dmysql=OFF      \
-      -Dopengl=OFF -Doracle=OFF -Dpgsql=OFF -Dpythia6=OFF -Dpythia8=OFF        \
-      -Dpyroot=OFF -Dpyroot_experimental=OFF -Droofit=OFF -Droot7=ON           \
-      -Droottest=OFF -Dspectrum=OFF -Dsqlite=OFF -Dssl=OFF -Dtesting=ON        \
-      -Dthreads=ON -Dtmva=OFF -Dvdt=OFF -Dwebgui=OFF -Dx11=OFF -Dxml=OFF       \
-      -Dxrootd=OFF                                                             \
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_STANDARD=${ROOT_CXX_STD}   \
+      -Dasimage=OFF -Dclad=OFF -Ddavix=OFF -Ddev=OFF -Dfail-on-missing=ON      \
+      -Dfftw3=OFF -Dfitsio=OFF -Dgdml=OFF -Dgfal=OFF -Dhttp=OFF -Dmlp=OFF      \
+      -Dmysql=OFF -Dopengl=OFF -Doracle=OFF -Dpgsql=OFF -Dpythia6=OFF          \
+      -Dpythia8=OFF -Dpyroot=OFF -Dpyroot_experimental=OFF -Droofit=OFF        \
+      -Droot7=ON -Droottest=OFF -Dspectrum=OFF -Dsqlite=OFF -Dssl=OFF          \
+      -Dtesting=ON -Dthreads=ON -Dtmva=OFF -Dvdt=OFF -Dwebgui=OFF -Dx11=OFF    \
+      -Dxml=OFF -Dxrootd=OFF                                                   \
       /mnt/root
 
 # Build ROOT
